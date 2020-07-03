381c86b5ca4fd6818ffd99aab30f19fe4b6dca14
rexetorize <58749843+rexetorize@users.noreply.github.com>
2020-07-03 18:28:57 +0530

    index files

---
+++ index.js
@@ -0,0 +1,25 @@
+let express = require("express");
+let socket = require("socket.io");
+
+let app = express();
+app.set("port", process.env.PORT || 5000);
+app.use(express.static("./public"));
+
+let server = app.listen(app.get("port"), function () {
+  console.log("Listening to port 4000");
+});
+
+let io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log("Connection -->" + socket.id);
+
+  socket.on("text", function (data) {
+    console.log(data);
+    io.sockets.emit("text", data);
+  });
+
+  socket.on("typing", function (data) {
+    socket.broadcast.emit("typing", data);
+  });
+});
